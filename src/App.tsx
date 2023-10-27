4da00e8a0ddc8487094ada6e5beb4e8762b735c4
Sunaidy <sunaidy1996@gmail.com>
2023-10-27 13:27:03 -0400

    mejorando tipado

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function ProductCategoryRow({ category }: any) {
   return (
     <tr>
-      <th colSpan="2">
+      <th colSpan={2}>
         {category}
       </th>
     </tr>
